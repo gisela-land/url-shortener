59ce182398d66b7e02cf48928a8960abc8c7eb0c
gisela-land <takeabreathe@hotmail.com>
2021-07-03 17:39:28 +0800

    feat: add Home page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use('/public', express.static('./public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
