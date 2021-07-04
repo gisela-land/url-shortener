c5d62eeae7169f14daf823e70f1fef103a8da65a
gisela-land <takeabreathe@hotmail.com>
2021-07-04 10:24:38 +0800

    feat: add shorten function & show page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+require('./config/mongoose.js')
+const ShortUrl = require('./models/url.js')
 
 const app = express()
 const port = 3000
@@ -9,11 +11,6 @@ app.set('view engine', 'hbs')
 
 app.use('/public', express.static('./public'))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-/*
 const wordbag = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
   'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -22,19 +19,45 @@ const wordbag = [
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ]
 
-function test() {
+function getShortenWords() {
   let d = Date.now();
   console.log('***', d)
   return 'xxxxx'.replace(/x/g, function (c) {
     const r = (d + Math.random() * wordbag.length) % wordbag.length | 0;
-    console.log(r)
+    // console.log(r)
     d = Math.floor(d / wordbag.length);
-    console.log(d)
-    console.log(wordbag[r])
+    // console.log(d)
+    // console.log(wordbag[r])
     return wordbag[r]
   })
 }
-*/
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/shorten', (req, res) => {
+  const oriUrl = req.query.originUrl.toLowerCase()
+  const host = req.headers.host
+  let shortUrl = ''
+  return ShortUrl.find({ url: oriUrl })
+    .lean()
+    .then((urls) => {
+      if (urls.length === 0) {
+        shortUrl = getShortenWords()
+        ShortUrl.create({
+          url: oriUrl,
+          shorten: shortUrl
+        })
+      } else {
+        shortUrl = urls[0].shorten
+      }
+      console.log('--- ', shortUrl)
+    })
+    .then(() => {
+      return res.render('show', { originUrl: oriUrl, host: host, shorten: shortUrl })
+    })
+})
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
